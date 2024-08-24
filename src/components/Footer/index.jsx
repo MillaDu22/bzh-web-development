3181330172a219bf1e1a4feaccb74abcfc4772b6
Milla22 <marti.ludmilla@orange.fr>
2024-08-24 21:56:29 +0200

    app implementée, css a ameliorer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,32 +1,40 @@
+import React, { useState } from 'react';
 import Mentions from '../Mentions/index.jsx';
 import Policy from '../Policy/index.jsx';
 import Terms from '../Terms/index.jsx';
 import './footer.css';
 
 function Footer() {
-    return(
-        <div className = "container-footer">
-            <div className ="box-links-footer">
-                <div className= "txt-footer">
-                    <p className = "adress">22 200 Guingamp - Bretagne - FRANCE</p>
-                    <p className = "copyright">©Ludmilla-Marti-2024</p> 
+    const [visibleComponent, setVisibleComponent] = useState(null);
+
+    const handleLinkClick = (component) => {
+        setVisibleComponent(visibleComponent === component ? null : component);
+    };
+
+    return (
+        <div className="container-footer">
+            <div className="box-links-footer">
+                <div className="txt-footer">
+                    <p className="adress">22 200 Guingamp - Bretagne - FRANCE</p>
+                    <p className="copyright">©Ludmilla-Marti-2024</p>
                 </div>
-                <div className ="container-social">
+                <div className="container-social">
                     <a className="facebook" href="/" target="_blank"><i className="fa-brands fa-facebook-f"></i></a>
                     <a className="linkedin" href="/" target="blank"><i className="fa-brands fa-linkedin-in"></i></a>
                     <a className="mail" href="mailto:marti.ludmilla@orange.fr" target="blank"><i className="fa-solid fa-envelope"></i></a>
                     <a className="insta" href="/" target="blank"><i className="fa-brands fa-instagram"></i></a>
                 </div>
                 <div className="legal-notice">
-                    <p><a href="#privacy-policy">Politique de confidentialité</a></p>
-                    <p><a href="#terms-of-service">Conditions d'utilisation</a></p>
-                    <p><a href="#legal-notice">Mentions légales</a></p>
+                    <p><a href="#privacy-policy" onClick={() => handleLinkClick('policy')}>Politique de confidentialité</a></p>
+                    <p><a href="#terms-of-service" onClick={() => handleLinkClick('terms')}>Conditions d'utilisation</a></p>
+                    <p><a href="#legal-notice" onClick={() => handleLinkClick('mentions')}>Mentions légales</a></p>
                 </div>
             </div>
-            <Mentions />
-            <Policy />
-            <Terms />
+            {visibleComponent === 'mentions' && <Mentions />}
+            {visibleComponent === 'policy' && <Policy />}
+            {visibleComponent === 'terms' && <Terms />}
         </div>
-    )
+    );
 }
+
 export default Footer;

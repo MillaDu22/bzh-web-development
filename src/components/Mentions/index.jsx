53f2c5b1fa6320a929c6ba9dfe4583c5eaa920ee
Milla22 <marti.ludmilla@orange.fr>
2024-09-09 10:57:31 +0200

    rectif numéro de tel

--- src/components/Mentions/index.jsx
+++ src/components/Mentions/index.jsx
@@ -5,8 +5,8 @@ function Mentions() {
         <section id="legal-notice" className="legal-notice-section">
             <h2>Mentions Légales</h2>
             <p><strong>Éditeur du Site :</strong> Ludmilla Marti</p>
-            <p><strong>Adresse :</strong> 22 200 Guingamp - Bretagne - FRANCE</p>
-            <p><strong>Téléphone :</strong> +33 6 58 06 28 00</p>
+            <p><strong>Adresse :</strong> 22 720 Plésidy - Bretagne - FRANCE</p>
+            <p><strong>Téléphone :</strong> +33 7 64 00 65 31</p>
             <p><strong>Email :</strong> <a href="mailto:armorwebcreations@gmail.com">armorwebcreations@gmail.com</a></p>
             <p><strong>Directeur de la publication :</strong> Ludmilla Marti</p>
             <p><strong>Hébergement :</strong> Vercel, https://vercel.com </p>

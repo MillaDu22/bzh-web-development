fd2cca2eb11c41f65401754374e21f165123eecf
Milla22 <marti.ludmilla@orange.fr>
2025-03-03 09:43:49 +0100

    Ajout article 101 au blog, maj promo MARS 2025

--- src/components/Promo/index.jsx
+++ src/components/Promo/index.jsx
@@ -18,8 +18,8 @@ function Promo() {
         premium: (originalPrices.premium * (1 - discountRate)).toFixed(2)
     };*/
 
-    const promoStartDate = "01/02/2025";
-    const promoEndDate = "28/02/2025";
+    const promoStartDate = "01/03/2025";
+    const promoEndDate = "31/03/2025";
 
     useEffect(() => {
         const section = document.querySelector('.container-promo');

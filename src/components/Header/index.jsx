bd19dc170c9bb4189dc4959693d454d783bfa095
Milla22 <marti.ludmilla@orange.fr>
2025-06-23 11:29:14 +0200

    Ajout photo Ludmilla page d'accueil'

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import Screens from '../../assets/images/screens.webp';
-import Logo3Video from '../Logo3/index.jsx';
+/*import Logo3Video from '../Logo3/index.jsx';*/
+import Me from '../../assets/images/ludmilla-AWC.webp';
+import RightIcon from '../../assets/images/—Pngtree—blue arrow abstract icon design_5951628.png';
 import './header.css';
 
 function Header() {
@@ -7,20 +9,10 @@ function Header() {
         <header className="container-hero">
             <div className="box-hero">
                 <div className="container-txt">
-                    {/*<h3 className="we-are">
-                        <i className="fas fa-laptop-code" aria-hidden="true" style={{ marginRight: '10px', color: '#FFD700' }}></i>
-                        Concepteur site web
-                    </h3>
-                    <h4 className="title-hero">
-                    <i className="fas fa-code" aria-hidden="true" style={{ marginRight: '10px', color: '#B0B0B0' }}></i>
-                        Développeur logiciel
-                    </h4>
-                    <p className="subtitle-hero">
-                    <i className="fas fa-lightbulb" aria-hidden="true" style={{ marginRight: '10px', color: '#FFD700' }}></i>
-                        Solutions du digital
-                    </p>*/}
-                    <Logo3Video />
+                    <img src= {Me} alt ="Ludmilla de Armor Web Créations" className ="meawc" />
+                    {/*<Logo3Video />*/}
                 </div>
+                <img src = {RightIcon} alt= "flèche de Ludmilla vers preview" className="right-icon" />
                 <div className="container-screens">
                     <img className="asset-hero" src={Screens} alt="preview responsive websites" />
                 </div>
